a1c5ceb8aebf61df081da3e37b00c079328a41f0
valeriemorales <vmorales4297@stu.d214.org>
2023-12-07 11:14:00 -0600

    Initial Commit

---
+++ PlannerApp/PlannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerAppApp.swift
+//  PlannerApp
+//
+//  Created by Valerie Morales on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PlannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
