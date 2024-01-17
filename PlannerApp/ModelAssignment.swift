822331f665cceb93a6a99e54907aeb97989a1ad9
Valeria Morales <vmorales4297@stu.d214.org>
2024-01-17 11:15:29 -0600

    took off some unecessary variables in model

--- PlannerApp/ModelAssignment.swift
+++ PlannerApp/ModelAssignment.swift
@@ -10,15 +10,11 @@ import Foundation
 class Assignment {
     var nameAssign : String
     var typeAssign : String
-    var timeAssign : Date
     var className : String
-    var reminder : Bool
     
-    init(nameAssign: String, typeAssign: String, timeAssign: Date, className: String, reminder: Bool) {
+    init(nameAssign: String, typeAssign: String, className: String) {
         self.nameAssign = nameAssign
         self.typeAssign = typeAssign
-        self.timeAssign = timeAssign
         self.className = className
-        self.reminder = reminder
     }
 }
