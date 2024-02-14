86ada89d43488795d8a01600a470058b9421f477
Jia Jakubiec <jiajakubiec@gmail.com>
2024-02-14 10:26:55 -0600

    .

---
+++ PlannerApp/NotificationReminder.swift
@@ -0,0 +1,32 @@
+//
+//  NotificationReminder.swift
+//  PlannerApp
+//
+//  Created by Jia Jakubiec on 2/14/24.
+//
+
+import UIKit
+
+
+
+
+//extension SceneDelegate {
+//    private  func triggerNotif(title: String, body: String) {
+//        let content = UNMutableNotificationContent()
+//        content.title = title
+//        content.body = body
+//        content.sound = .default
+//        content.subtitle = "Reminder!"
+//        
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//        let localNRequest = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+//        
+//        UNUserNotificationCenter.current().add(localNRequest) {
+//            if let error = error {
+//                print("Error: ", error.localizedDescription)
+//            } else {
+//                NSLog("Notification scheduled")
+//            }
+//        }
+//    }
+//}
