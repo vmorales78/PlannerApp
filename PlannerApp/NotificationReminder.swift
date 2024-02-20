fc31021ef97a2f4962651698d440c16bf919d4c5
Jia Jakubiec <jiajakubiec@gmail.com>
2024-02-20 11:22:35 -0600

    Made "Allow Notification?" view show up when @UIApplicationMain was used.

--- PlannerApp/NotificationReminder.swift
+++ PlannerApp/NotificationReminder.swift
@@ -7,26 +7,50 @@
 
 import UIKit
 
+//@UIApplicationMain
 
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        usernotificationConfig()
+        return true
+    }
+    func application(_ application: UIApplication, configureForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        //
+    }
+}
 
+extension AppDelegate {
+    private func usernotificationConfig() {
+        UNUserNotificationCenter.current().requestAuthorization(options:[.alert, .sound]){
+            (isApproved, error) in
+            if isApproved {
+                print("User Notification: Approved")
+                UNUserNotificationCenter.current().delegate = self
+            } else {
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func notificationSnooze() {
+        let snoozeButton = UNNotificationAction(identifier: "snooze", title: "Snooze", options: .init(rawValue: 0))
+        let snoozeCategory = UNNotificationCategory(identifier: "snooze_category", actions: [snoozeButton], intentIdentifiers: [], hiddenPreviewsBodyPlaceholder: "", options: .customDismissAction)
+        UNUserNotificationCenter.current().setNotificationCategories([snoozeCategory])
+    }
+}
 
-//extension SceneDelegate {
-//    private  func triggerNotif(title: String, body: String) {
-//        let content = UNMutableNotificationContent()
-//        content.title = title
-//        content.body = body
-//        content.sound = .default
-//        content.subtitle = "Reminder!"
-//        
-//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-//        let localNRequest = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
-//        
-//        UNUserNotificationCenter.current().add(localNRequest) {
-//            if let error = error {
-//                print("Error: ", error.localizedDescription)
-//            } else {
-//                NSLog("Notification scheduled")
-//            }
-//        }
-//    }
-//}
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, withCompletionHandler completionHandler: @escaping(UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping() -> Void) {
+        print("Usernotification didReceive: \(response)")
+        completionHandler()
+    }
+}
+
--- PlannerApp/NotificationsView.swift
+++ PlannerApp/NotificationsView.swift
@@ -5,4 +5,59 @@
 //  Created by Jia Jakubiec on 2/6/24.
 //
 
-import Foundation
+//import Foundation
+//import UIKit
+//import UserNotifications
+//
+//class NotificationsView: UIViewController {
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//    
+//    func checkForPermission() {
+//        let notificationCenter = UNUserNotificationCenter.current()
+//        notificationCenter.getNotificationSettings { settings in
+//            switch settings.authorizationStatus {
+//            case .authorized:
+//                self.dispatchNotification()
+//            case .denied:
+//                return
+//            case .notDetermined:
+//                notificationCenter.requestAuthorization(options: [.alert, .sound]) { didAllow, error in
+//                    if didAllow {
+//                        self.dispatchNotification()
+//                    }
+//                }
+//            default:
+//                return
+//            }
+//        }
+//    }
+//    
+//    func dispatchNotification() {
+//        let identifier = "my-notification"
+//        let title = "Check Assignment!"
+//        let body = "You have something due soon."
+//        //let hour = 11
+//        //let minute = 20
+//        
+//        let notificationCenter = UNUserNotificationCenter.current()
+//        
+//        let content = UNMutableNotificationContent()
+//        content.title = title
+//        content.body = body
+//        content.sound = .default
+//        
+//        let calendar = Calendar.current
+//        var dataComponents = DataComponents(calendar: calendar, timezone: TimeZone.current)
+//        //dataComponents.hour = hour
+//        //dataComponents.minute = minute
+//        
+//        let trigger = UNCalendarNotificationTrigger(coder: dataComponents)
+//        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+//        
+//        notificationCenter.removePendingNotificationRequests(withIdentifiers: [identifier])
+//        notificationCenter.add(request)
+//    }
+//}
