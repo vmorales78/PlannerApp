10fdeaac0f03fb5de9b4a50b09619a4dbc9d911d
coolvinice <ramen.calvin@yahoo.com>
2024-03-05 10:59:01 -0600

    Merging commits

--- PlannerApp/CalendarView.swift
+++ PlannerApp/CalendarView.swift
@@ -152,4 +152,34 @@ extension DayRangeIndicatorView: CalendarItemViewRepresentable {
     
 }
 
+struct theDay: CalendarItemViewRepresentable {
+    struct InvariantViewProperties: Hashable {
+        let font: UIFont
+        let textColor: UIColor
+        let backgroundColor: UIColor
+    }
+    struct Content: Equatable {
+        let day: DayComponents
+    }
+    static func makeView(
+        withInvariantViewProperties invariantViewProperties: InvariantViewProperties)
+    -> UILabel
+    {
+        let label = UILabel()
+        
+        label.backgroundColor = invariantViewProperties.backgroundColor
+        label.font = invariantViewProperties.font
+        label.textColor = invariantViewProperties.textColor
+        
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 12
+        
+        return label
+    }
+    static func setContent(_ content: Content, on view: UILabel) {
+        view.text = "\(content.day.day)"
+    }
+    
+}
 
